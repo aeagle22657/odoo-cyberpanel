dcfa2a2543ab37406e617ba8002000553880031b
aeagle22657 <93518197+aeagle22657@users.noreply.github.com>
2022-03-30 22:59:48 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,8 @@
-/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8090
+#!/bin/bash
+
+START_DIR="${START_DIR:-/}"
+
+PREFIX="deploy-code-server"
+
+mkdir -p $START_DIR
+/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8090 $START_DIR
